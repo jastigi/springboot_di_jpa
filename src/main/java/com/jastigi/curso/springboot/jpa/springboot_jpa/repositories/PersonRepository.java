e3114089d562ea3e831a7cee6c8ad0d899e5bb14
jastigi <jastigi@gmail.com>
2026-01-26 23:09:59 +0100

    Primer commit JPA desde Antigravity

--- src/main/java/com/jastigi/curso/springboot/jpa/springboot_jpa/repositories/PersonRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jastigi.curso.springboot.jpa.springboot_jpa.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.jastigi.curso.springboot.jpa.springboot_jpa.entities.Person;
-
-public interface PersonRepository extends CrudRepository<Person, Long> {
-
-}
